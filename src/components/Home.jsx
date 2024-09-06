691104a27e2f4b82b5b422e96266b7353257b57b
joyegwuatu <egwuatujoy60@gmail.com>
2024-09-06 12:53:03 +0100

    updated files

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,14 +2,14 @@ const Home = () => {
   return (
     <section className="w-full bg-lightgray">
       <div className=" w-full bg-[url('/public/images/home-img.jpg')] bg-no-repeat bg-contain    ">
-        <div className="text-white text-center py-56 px-44 ">
+        <div className="text-white text-center py-56  ">
           <h2 className="text-neutral-500  text-sm tracking-overWide mb-5 ">
             NEW PRODUCT
           </h2>
           <h1 className="text-4xl  font-semibold  mb-5">
             XX99 MARK II HEADPHONES
           </h1>
-          <p className="text-xs mb-8">
+          <p className="text-xs mb-8 px-16">
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
           </p>
