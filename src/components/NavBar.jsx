42ff36d91df0f0231f8bc19dcf69b036931f9f1f
joyegwuatu <egwuatujoy60@gmail.com>
2024-09-11 13:36:16 +0100

    updated files

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,10 +6,7 @@ const NavBar = () => {
   return (
     <header className="bg-black text-white ">
       <nav className="flex justify-between px-10 py-8 items-center ">
-      
         <div className="flex justify-center items-center gap-8  overflow-hidden ">
-
-
           <button onClick={() => setToggle(!toggle)}>
             <img src="/images/Group.png" alt="" />
           </button>
@@ -19,33 +16,32 @@ const NavBar = () => {
           </div>
 
           <div
-            className={`absolute left-0  top-20 h-0  w-full bg-black  ease-in duration-75 ${
-              toggle ? "h-1/2" : "h-0"
+            className={`absolute left-0  top-20 h-0 w-full bg-black ease-linear duration-75 ${
+              toggle ? "h-1/2  " : "h-0 "
             }`}
           >
-            <ul className={toggle ? "block" : "hidden"}>
-              <li className=" p-7  hover:bg-lightgray hover:text-orange ">
-                <a href="#home">HOME</a>
-              </li>
-              <li className=" p-7 hover:bg-lightgray hover:text-orange">
-                <a href="#recipe">HEADPHONES</a>
-              </li>
-              <li className=" p-7 hover:bg-lightgray hover:text-orange">
-                <a href="#popular">SPEAKERS</a>
-              </li>
-              <li className=" p-7  hover:bg-lightgray hover:text-orange">
-                <a href="#delivery">EARPHONES</a>
-              </li>
-            </ul>
+            {toggle && (
+              <ul>
+                <li className=" p-7  hover:bg-lightgray hover:text-orange ">
+                  <a href="#home">HOME</a>
+                </li>
+                <li className=" p-7 hover:bg-lightgray hover:text-orange">
+                  <a href="#recipe">HEADPHONES</a>
+                </li>
+                <li className=" p-7 hover:bg-lightgray hover:text-orange">
+                  <a href="#popular">SPEAKERS</a>
+                </li>
+                <li className=" p-7  hover:bg-lightgray hover:text-orange">
+                  <a href="#delivery">EARPHONES</a>
+                </li>
+              </ul>
+            )}
           </div>
-
-
         </div>
 
         <div>
           <img src="/images/Cart.svg" alt="" />
         </div>
-
       </nav>
     </header>
   );
